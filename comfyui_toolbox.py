e0a4df3480c0f22e55233705692eed4fc3c7d2e7
zcfrank1st <zcfrank1st@foxmail.com>
2023-12-09 16:34:53 +0800

    fix

--- comfyui_toolbox.py
+++ comfyui_toolbox.py
@@ -1,4 +1,11 @@
 import json
+import folder_paths
+import os
+from pathlib import Path
+import random
+
+JSON_OUT_PATH = os.path.join(folder_paths.output_directory, "json")
+Path(JSON_OUT_PATH).mkdir(parents=True, exist_ok=True)
 
 class TestJsonPreview:
     @classmethod
@@ -16,7 +23,36 @@ class TestJsonPreview:
     def json_test(self, text):
         return (text, )
 
-class PreviewJson:
+class SaveJson:
+    def __init__(self):
+        self.output_dir = JSON_OUT_PATH
+        self.prefix_append = ""
+    
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "filename": ("STRING",),
+                "json_content": ("JSON",),
+            },
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "save_json"
+    OUTPUT_NODE = True
+    CATEGORY = "toolbox"
+
+    def save_json(self, filename, json_content):
+        pretty_json = json.dumps(json_content, indent=4)
+        with open(Path(self.output_dir) / f"{filename}{self.prefix_append}.json", "w") as outfile:
+            outfile.write(pretty_json)
+        return { "ui": { "json_file": f"{filename}{self.prefix_append}.json" }}
+
+class PreviewJson(SaveJson):
+    def __init__(self):
+        self.output_dir = folder_paths.get_temp_directory()
+        self.prefix_append = "_temp_" + ''.join(random.choice("abcdefghijklmnopqrstupvxyz") for x in range(5))
+    
     @classmethod
     def INPUT_TYPES(s):
         return {
@@ -25,33 +61,21 @@ class PreviewJson:
             },
         }
 
-    RETURN_TYPES = ()
-    FUNCTION = "json_preview"
-    OUTPUT_NODE = True
-    CATEGORY = "toolbox"
-
-    def json_preview(self, json_content):
-        pretty_json = json.dumps(json_content, indent=4)
-        return { "ui": { "json_content": "helloworld" }}
-
-# class SaveJson:
-#     ...
-
 # class PreviewVideo:
 #     ...
 
 NODE_CLASS_MAPPINGS = {
-    "PreviewJson": PreviewJson,
     "TestJsonPreview": TestJsonPreview,
-    #"SaveJson": SaveJson,
+    "PreviewJson": PreviewJson,
+    "SaveJson": SaveJson,
     #"PreviewVideo": PreviewVideo
 }
 
 # A dictionary that contains the friendly/humanly readable titles for the nodes
 NODE_DISPLAY_NAME_MAPPINGS = {
     "PreviewJson": "Preview Json",
+    "SaveJson": "Save Json",
     "TestJsonPreview": "Test Json Preview",
-    #"SaveJson": "Save Json",
     #"PreviewVideo": "Preview Video"
 }
 
