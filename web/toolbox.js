ae2b5611c7336eb8823ee8e20278d721cedd2247
zcfrank1st <zcfrank1st@foxmail.com>
2023-12-10 13:47:47 +0800

    test fontend

--- web/toolbox.js
+++ web/toolbox.js
@@ -2,103 +2,39 @@ import { app } from "/scripts/app.js";
 import "/scripts/domWidget.js"
 import {ComfyWidgets} from "/scripts/widgets.js"
 import { api } from "/scripts/api.js";
+import { $el } from "/scripts/ui.js";
+
+function get_position_style(ctx, widget_width, y, node_height) {
+    const MARGIN = 4;  // the margin around the html element
+
+/* Create a transform that deals with all the scrolling and zooming */
+    const elRect = ctx.canvas.getBoundingClientRect();
+    const transform = new DOMMatrix()
+        .scaleSelf(elRect.width / ctx.canvas.width, elRect.height / ctx.canvas.height)
+        .multiplySelf(ctx.getTransform())
+        .translateSelf(MARGIN, MARGIN + y);
+
+    return {
+        transformOrigin: '0 0',
+        transform: transform,
+        left: `0px`, 
+        top: `0px`,
+        position: "absolute",
+        maxWidth: `${widget_width - MARGIN*2}px`,
+        maxHeight: `${node_height - MARGIN*2}px`,    // we're assuming we have the whole height of the node
+        width: `auto`,
+        height: `auto`,
+    }
+}
 
 const ext = {
-	// Unique name for the extension
 	name: "Comfyui.ToolboxExtension",
-	// async init(app) {
-	// 	// Any initial setup to run as soon as the page loads
-	// },
-	// async setup(app) {
-	// 	// Any setup to run after the app is created
-	// },
-	// async addCustomNodeDefs(defs, app) {
-	// 	// Add custom node definitions
-	// 	// These definitions will be configured and registered automatically
-	// },
-	async getCustomWidgets(app) {
-		// Return custom widget types
-		// See ComfyWidgets for widget examples
-		// console.log("[logging]", "provide custom widgets");
-
-		return {
-			JSON(node, inputName, inputData, app) {
-				// const inputEl = document.createElement("code");
-				// inputEl.className = "comfy-json-preview";
-				// // inputEl.value = inputData.value;
-
-				// const widget = node.addDOMWidget(inputName, "customjson", inputEl, {
-				// 	getValue() {
-				// 		return inputEl.value;
-				// 	},
-				// 	setValue(v) {
-				// 		inputEl.value = v;
-				// 	},
-				// });
-				// widget.inputEl = inputEl;
-
-				// return { minWidth: 400, minHeight: 400, widget }
-				let defaultVal = "Json output...";
-				let options = {};
-				let wi = node.addWidget(
-					"customjson",
-					inputName,
-					defaultVal,
-					() => {},
-					options,
-					)
-				wi.type = "STRING"
-				return {
-					widget: wi
-				};
-			}
-		}
-
-		// TODO add link and preview code
-		// const defaultVal = inputData[1] || "";
-		// return { widget: node.addWidget("json", inputName, defaultVal, () => {}, {}) };
-		// } 
-		// }
-	},
-
 	async beforeRegisterNodeDef(nodeType, nodeData, app) {
-		// Run custom logic before a node definition is registered with the graph
 		if (nodeType.comfyClass === "PreviewJson") { // 3
-			// let ext = this;
-
-			// const onNodeCreated = nodeType.prototype.onNodeCreated;
-			// nodeType.prototype.onNodeCreated = function () {
-			// 	const ret = onNodeCreated
-			// 		? onNodeCreated.apply(this, arguments)
-			// 		: undefined;
-
-			// 	let PreviewJsonNode = app.graph._nodes.filter(
-			// 		(wi) => wi.type == nodeData.name
-			// 	),
-			// 		nodeName = `${nodeData.name}_${PreviewJsonNode.length}`;
-
-			// 	console.log(`Create ${nodeData.name}: ${nodeName}`);
-
-			// 	let currentNode = this;
-			// 	let createWi = async function() {
-			// 		const customWis = await ext.getCustomWidgets(app);
-			// 		let wi = customWis.JSON(
-			// 			currentNode,
-			// 			nodeName,
-			// 			{
-			// 				value: "Json output...",
-			// 			},
-			// 			app
-			// 		);
-			// 		wi.widget.inputEl.readOnly = true;
-			// 	}
-			// 	createWi();
-			// 	// return ret;
-			// };
 			const outSet = function (texts) {
 				if (texts.json_file.length > 0) {
 					let widget_id = this?.widgets.findIndex(
-						(w) => w.type == "customjson"
+						(w) => w.type == "showjson"
 					);
 					
 					let currentWidget = this.widgets[widget_id]
@@ -130,19 +66,36 @@ const ext = {
 				onExecuted?.apply(this, arguments);
 				outSet.call(this, texts);
 			};
+
+			const orig_nodeCreated = nodeType.prototype.onNodeCreated;
+            nodeType.prototype.onNodeCreated = function () {
+                orig_nodeCreated?.apply(this, arguments);
+
+                const widget = {
+                    type: "showjson",   // whatever
+                    name: "showjson", // whatever
+                    draw(ctx, node, widget_width, y, widget_height) { 
+                        Object.assign(this.inputEl.style, get_position_style(ctx, widget_width, y, node.size[1])); // assign the required style when we are drawn
+                    },
+                };
+
+                /*
+                Create an html element and add it to the document.  
+                Look at $el in ui.js for all the options here
+                */
+                widget.inputEl = $el("code");
+                document.body.appendChild(widget.inputEl);
+
+                /*
+                Add the widget, make sure we clean up nicely, and we do not want to be serialized!
+                */
+                this.addCustomWidget(widget);
+                this.onRemoved = function () { widget.inputEl.remove(); };
+                this.serialize_widgets = false;
+
+            }
 		}
 	},
-	// async registerCustomNodes(app) {
-	// 	// Register any custom node implementations here allowing for more flexability than a custom node def
-	// },
-	// loadedGraphNode(node, app) {
-	// 	// Fires for each node when loading/dragging/etc a workflow json or png
-	// 	// If you break something in the backend and want to patch workflows in the frontend
-	// },
-	// nodeCreated(node, app) {
-	// 	// Fires every time a node is constructed
-	// 	// You can modify widgets/add handlers/etc here
-	// }
 };
 
 app.registerExtension(ext);
